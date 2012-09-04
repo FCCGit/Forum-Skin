88fb7c84e9219264fe6e81855670fdf80f8f8ea8
Taylor Jones <monitorjbl@gmail.com>
2012-09-04 00:55:51 -0400

    initial commit

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,10 @@
+<?php
+
+$txt['theme_welcome'] = 'Welcome, ';
+$txt['theme_messages'] = 'messages,';
+$txt['theme_unread'] = ' unread';
+$txt['theme_login'] = 'Login';
+$txt['theme_or'] = 'or';
+$txt['theme_register'] = 'Register';
+
+?>
---
+++ languages/ThemeStrings.polish-utf8.php
@@ -0,0 +1,10 @@
+<?php
+
+$txt['theme_welcome'] = 'Witaj, ';
+$txt['theme_messages'] = 'wiadomości,';
+$txt['theme_unread'] = ' nieprzeczytanych';
+$txt['theme_login'] = 'Zaloguj się';
+$txt['theme_or'] = 'lub';
+$txt['theme_register'] = 'Zarejestruj';
+
+?>
